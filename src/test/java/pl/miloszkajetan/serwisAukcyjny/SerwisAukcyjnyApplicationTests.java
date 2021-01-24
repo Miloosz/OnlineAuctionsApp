0f2e04259d090c75f657bb4dbd898c952e8678ec
milos <milosz.parada@gmail.com>
2021-01-24 09:51:09 +0100

    test

---
+++ src/test/java/pl/miloszkajetan/serwisAukcyjny/SerwisAukcyjnyApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.miloszkajetan.serwisAukcyjny;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SerwisAukcyjnyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
