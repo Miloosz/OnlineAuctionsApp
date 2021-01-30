6054a1f30fe290b1d67ca0d1807b3bef2e284e8e
milos <milosz.parada@gmail.com>
2021-01-30 15:46:09 +0100

    male zmiany w userze

--- src/main/java/pl/miloszkajetan/serwisAukcyjny/user/UserRegistrationBuilder.java
+++ src/main/java/pl/miloszkajetan/serwisAukcyjny/user/UserRegistrationBuilder.java
@@ -3,19 +3,16 @@ package pl.miloszkajetan.serwisAukcyjny.user;
 import lombok.Builder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-import java.time.Instant;
-import java.time.LocalDate;
-
 @Builder
 public class UserRegistrationBuilder {
 
     public static User userRegistration(UserRegistration userRegistration, PasswordEncoder passwordEncoder) {
 
-        User user = new User();
-        User.builder()
-                .login(user.getLogin())
-                .password(passwordEncoder.encode(user.getPassword()))
-                .email(user.getEmail())
+
+        return User.builder()
+                .login(userRegistration.getLogin())
+                .password(passwordEncoder.encode(userRegistration.getPassword()))
+                .email(userRegistration.getEmail())
                 .firstName(userRegistration.getFirstName())
                 .lastName(userRegistration.getLastName())
                 .phoneNumber(userRegistration.getPhoneNumber())
@@ -23,7 +20,7 @@ public class UserRegistrationBuilder {
                 .address(userRegistration.getAddress())
                 .userRole(UserRoleEnum.USER)
                 .build();
-        return null;
+
     }
 
 
