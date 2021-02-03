98600bfcdf95e362672c5a100842b5730f3fed96
milos <milosz.parada@gmail.com>
2021-02-03 17:42:58 +0100

    dodanie componentow

---
+++ angular-frontend/src/app/advertisments/advertisments.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-advertisments',
+  templateUrl: './advertisments.component.html',
+  styleUrls: ['./advertisments.component.css']
+})
+export class AdvertismentsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- angular-frontend/src/app/app.module.ts
+++ angular-frontend/src/app/app.module.ts
@@ -2,10 +2,28 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { MainMenuComponent } from './main-menu/main-menu.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { LoginComponent } from './login/login.component';
+import { CartComponent } from './cart/cart.component';
+import { CategoryComponent } from './category/category.component';
+import { AdvertismentsComponent } from './advertisments/advertisments.component';
+import { ContactComponent } from './contact/contact.component';
+import { ItemsComponent } from './items/items.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    MainMenuComponent,
+    NavigationComponent,
+    LoginComponent,
+    CartComponent,
+    CategoryComponent,
+    AdvertismentsComponent,
+    ContactComponent,
+    ItemsComponent
   ],
   imports: [
     BrowserModule
