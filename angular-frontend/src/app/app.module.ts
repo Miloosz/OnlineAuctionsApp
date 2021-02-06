a096495bf94fbc301d71321ac87f60cc25a1639f
KajetanAmos <kajetan.kotlowski@gmail.com>
2021-02-06 09:19:33 +0100

    product list and cart list

--- angular-frontend/src/app/app.module.ts
+++ angular-frontend/src/app/app.module.ts
@@ -6,13 +6,16 @@ import { HomeComponent } from './home/home.component';
 import { MainMenuComponent } from './main-menu/main-menu.component';
 import { NavigationComponent } from './navigation/navigation.component';
 import { LoginComponent } from './login/login.component';
-import { CartComponent } from './cart/cart.component';
+import { CartComponent } from './shopping-cart/cart/cart.component';
 import { CategoryComponent } from './category/category.component';
 import { AdvertismentsComponent } from './advertisments/advertisments.component';
 import { ContactComponent } from './contact/contact.component';
 import { ItemsComponent } from './items/items.component';
 import {MatGridListModule} from "@angular/material/grid-list";
 import {AppRoutingModule} from "./app-routing.module";
+import { ProductItemComponent } from './product/product-item/product-item.component';
+import { ProductListComponent } from './product/product-list/product-list.component';
+import { CartItemComponent } from './shopping-cart/cart-item/cart-item.component';
 
 
 
@@ -28,6 +31,9 @@ import {AppRoutingModule} from "./app-routing.module";
     AdvertismentsComponent,
     ContactComponent,
     ItemsComponent,
+    ProductItemComponent,
+    ProductListComponent,
+    CartItemComponent,
       ],
     imports: [
         BrowserModule,
